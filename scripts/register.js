c46300cdec3b573703885d0e4a83b5cf18711272
Ing. Eder Montes <amontesr1@hotmail.com>
2025-02-13 22:56:47 -0600

    Agregado endpoint de render en register.js

--- scripts/register.js
+++ scripts/register.js
@@ -16,7 +16,7 @@
 
         // Enviar los datos al backend
         try {
-            const response = await fetch('http://localhost:5000/api/auth/register', {
+            const response = await fetch('http://localhost:5000/api/auth/register' || 'https://pokemondex-hz6s.onrender.com/api/auth/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
