7a9c0e47eab46cb16b01ed1e5c2ba00e8984c2ff
Ing. Eder Montes <amontesr1@hotmail.com>
2025-03-09 19:32:04 -0600

    Arreglado Design en moviles

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 //Archivo de configuracion para saber si usar el entorno de desarrollo o el de produccion
 
-const isProduction = true; // Cambia a `true` cuando estés en producción
+const isProduction = false; // Cambia a `true` cuando estés en producción
 
 const config = {
     apiUrl: isProduction ? 'https://pokemondex-hz6s.onrender.com' : 'http://localhost:5000',
